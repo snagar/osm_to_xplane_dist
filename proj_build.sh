fd258ba9f2d9a4079698c75b53ce317ecdc5a975
snagar <snagar.dev@protonmail.com>
2025-08-09 15:49:56 +0300

    Added proj_build.sh as a wrapper for pyinstaller.

---
+++ proj_build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+source .venv/bin/activate
+
+## Create the destination folder
+mkdir -p dist/osm_to_xplane/
+
+## Step 1: pack the project
+echo -e "Packing Python Project\n"
+
+if [ -z "$VIRTUAL_ENV_PROMPT" ]; then
+    echo "no value is set for $VIRTUAL_ENV_PROMPT"
+    pyinstaller --clean --noconfirm osm_to_xplane.py
+else
+    echo "VIRTUAL_ENV_PROMPT is set to: $VIRTUAL_ENV_PROMPT"
+    "$VIRTUAL_ENV_PROMPT/bin/pyinstaller" --clean --noconfirm osm_to_xplane.py
+fi
+
+
+## Step 2: Copy the needed files into the project
+echo -e "\nPost Pack - Copying the missing files\n"
+
+# Copy mandatory files
+cp -p README-FIRST_TIME.md dist/osm_to_xplane/
+cp -p README-FIRST_TIME.md dist/osm_to_xplane/README-FIRST-TIME.txt
+cp -p config*.json dist/osm_to_xplane/
+rm -f dist/osm_to_xplane/config.json
+cp -p uv_xml_config.xml dist/osm_to_xplane/
+cp -p dsf_template.tmpl dist/osm_to_xplane/
+# Copy folders and clean them
+cp -rp tools dist/osm_to_xplane
+rm -f  dist/osm_to_xplane/tools/dsf_obj8*
+
+cp -rp blender dist/osm_to_xplane/
+cp -rp textures dist/osm_to_xplane/
+
+exit 0;
